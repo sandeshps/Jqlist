466e17407451ddc77c428afe4cdd46e6ac96e09f
sandeshps <sandeshps1989@yahoo.com>
2014-06-16 19:45:58 +0530

    Issue with click function

--- script.js
+++ script.js
@@ -2,32 +2,32 @@
  * Created by sandesh on 6/16/2014.
  */
 
-var ulListID = 'list'; // Unordered list ID for further use 
 
 $(document).ready(function (){
     createUList(); // Need to call only once. 
     $('button').on('click',function() {
         $textValue = $("#textBox").val();
-        addList($textValue);
+        //addListItem($textValue);
+        
     });
 });
 
 
-// Function that will add the list
-function addList($text){    
-    $listValue = "<li>" + $text + "</li>";
-    $("#list").append($listValue); // Add list item to the unordered list    
-}
+/
 
 
-// Function to create an Unordered List
+// Function to create an Unordered List. Call this only once
 function createUList() {
-    $ulList = "<ul> id='"+ulListID+"'></ul>";
+    $ulList = "<ul> id='list'></ul>";
     $("#dynamicList").append($ulList);
 }
 
 
-function createListItem() {
+// Function to add list item and button to the <div> id = list 
+function addListItem($text) {
+   
+    $listValue = "<li>" + $text + "</li>";
+    $("#list").append($listValue); // Add list item to the unordered list    
 }
 
 
