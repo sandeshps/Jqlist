b226946caa22c93b51bcdccabf40197b0b2a65cd
sandeshps <sandeshps1989@yahoo.com>
2014-06-16 22:21:06 +0530

    Fixed ready function issue

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
  * Created by sandesh on 6/16/2014.
  */
 
+var $count = 0; // For counting the number of lists
+var $buttonText = "btnList"; // Fixed string for every button created dynamically. Random number will be appended to this to get different IDs
+var $buttons = []; // To store the dynamically created buttons
 
 
 $(document).ready(function () {
@@ -17,17 +20,19 @@ $(document).ready(function () {
 
 // Function to create an Unordered List. Call this only once
 function createUList() {
-    $ulList = "<ul> id='list'></ul>";
-    //$("#dynamicList").append($ulList);
-    ($ulList).insertAfter("#dynamicList");
+    $ulList = "<ul id='list'></ul>";
+    $("#dynamicList").append($ulList);
 }
 
 
 // Function to add list item and button to the <div> id = list 
-function addListItem($text) {   
-    $listValue = "<li>" + $text + "</li>";
-    //$("#list").append($listValue); // Add list item to the unordered list    
-    ($listValue).insertAfter("#list");
+function addListItem($text) {       
+    $buttonWithID = $buttonText + Math.random(0,Math.random(1,1000000)).toFixed(5);
+    $buttons.push($buttonWithID); // We need this later to identify the button clicked
+    $button = "<input type='button' id='" + $buttonWithID +"' value='Delete' >";
+    $listValue = "<li>" + $text + " " + $button + "</li>";
+    $("#list").append($listValue); // Add list item to the unordered list   
+    
 }
 
 
