27d7cf54022f0bf8bd683533b5b991b9f109cc8b
sandeshps <sandeshps1989@yahoo.com>
2014-06-16 19:33:07 +0530

    Fixed creation of unordered list

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
  * Created by sandesh on 6/16/2014.
  */
 
+var ulListID = 'list'; // Unordered list ID for further use 
 
 $(document).ready(function (){
+    createUList(); // Need to call only once. 
     $('button').on('click',function() {
         $textValue = $("#textBox").val();
         addList($textValue);
@@ -12,11 +14,20 @@ $(document).ready(function (){
 
 
 // Function that will add the list
-function addList($text){
-    $listElement = "<ul id='list'></ul>"; // Unordered list
-    $("#dynamicList").append("<ul id='list'></ul>"); // Append Unordered list to the <div>
+function addList($text){    
     $listValue = "<li>" + $text + "</li>";
     $("#list").append($listValue); // Add list item to the unordered list    
+}
+
+
+// Function to create an Unordered List
+function createUList() {
+    $ulList = "<ul> id='"+ulListID+"'></ul>";
+    $("#dynamicList").append($ulList);
+}
+
+
+function createListItem() {
 }
 
 
