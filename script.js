baa7560a3f60c3bdb162daf9c39f0b7921a07908
sandeshps <sandeshps1989@yahoo.com>
2014-06-17 13:58:07 +0530

    Adding code for event click on each button at run time

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var $count = 0; // For counting the number of lists
 var $buttonText = "btnList"; // Fixed string button created dynamically. Random number will be appended to get different IDs
 var $idText = "item"; // Fixed string list item created dynamically. Random number will be appended to get different list IDs 
 var $buttons = []; // To store the dynamically created buttons
+// To store each and every 'list id' corresponding to the 'button id'. To identify the button under a list item
+var $listID = [];
+var $buttonID = [];
 
 
 
@@ -33,22 +36,25 @@ function createUList() {
 function addListItem($text) {       
     var $textBox = $("#textBox"); // To clear the textbox
     $textBox.val(" ");
-    var $buttonWithID = $buttonText + Math.random(0,Math.random(1,1000000)).toFixed(5); // Generate a random ID to append with the button text
+    var $buttonWithID = $buttonText + Math.random(0,Math.random(1,1000000)).toFixed(5); // Generate a random ID to append with the 'button' text
     $buttons.push('#'+$buttonWithID); // We need this later to identify the button clicked
     var $button = "<input type='button' id='" + $buttonWithID +"' value='Delete' >";
-    var $listItem = "<li>" + $text + " " + $button + "</li>"; 
+    var $idWithID = $idText + Math.random(0,Math.random(1,1000000)).toFixed(5); // Generate a random ID to append with the 'list' text
+    var $listItem = "<li id='" + $idWithID + "'>" + $text + " " + $button + "</li>"; 
     $("#list").append($listItem); // Add list item with button to the unordered list      
-    attachEvents('#'+$buttonWithID);
-    
-    
+    $listID.push('#' + $idWithID);
+    $buttonID.push('#' + $buttonWithID);
+    attachEvents('#' + $buttonWithID);
 }
 
 
 
 // Attach 'click' even to each button generated 
 function attachEvents($btn) {
-    $b = $btn;
-    $("#dynamicList").on('click',$btn,deleteListItem);
+    //alert($.data(object,'events'));
+    for(var $i=0; i < $buttonID.length ; i++) {
+        $("#dynamicList").on('click',$buttonID[i],deleteListItem);
+    }
          
 }
 
